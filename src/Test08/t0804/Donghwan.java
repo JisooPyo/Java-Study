2d77abd1ef23dffd1b8aeae66e553ec94175d56b
alxqkekrroqkfwk <fexxet@icloud.com>
2023-08-04 16:59:58 +0900

    init

--- src/Test08/t0804/Donghwan.java
+++ src/Test08/t0804/Donghwan.java
@@ -1,4 +1,27 @@
 package Test08.t0804;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Donghwan {
+    public static void main(String[] args) {
+        Donghwan donghwan = new Donghwan();
+
+        System.out.println(Arrays.toString(donghwan.solution("banana")));
+        System.out.println(Arrays.toString(donghwan.solution("foobar")));
+    }
+
+    public int[] solution(String s) {
+        int[] arr = new int[s.length()];
+
+        for (int i=0; i<s.length(); i++) {
+            if (s.indexOf(s.charAt(i))== i) {
+                arr[i] = -1;
+                System.out.println(arr[i]);
+            }else {
+                arr[i] = i - s.indexOf(s.charAt(i));
+            }
+        }
+        return arr;
+    }
 }
