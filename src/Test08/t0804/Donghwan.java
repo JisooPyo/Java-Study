a5d9ccd8eb6db024f404ffa7d9307942da58deb7
alxqkekrroqkfwk <133636240+alxqkekrroqkfwk@users.noreply.github.com>
2023-08-04 17:10:37 +0900

    Update Donghwan.java

--- src/Test08/t0804/Donghwan.java
+++ src/Test08/t0804/Donghwan.java
@@ -13,13 +13,15 @@ public class Donghwan {
 
     public int[] solution(String s) {
         int[] arr = new int[s.length()]; // s의 길이만큼 int 배열 생성
+        ArrayList<Character> arrayList= new ArrayList<>();
 
         for (int i=0; i<s.length(); i++) { // s의 길이만큼 반복
             if (s.indexOf(s.charAt(i))== i) { // indexOf로 s.charAt으로 찾은 알파벳의 위치가 i와 대조
                 arr[i] = -1; // 처음 들어온 값이라면 arr[i]에 값 저장.
             }else {
-                arr[i] = i - s.indexOf(s.charAt(i)); // 중복된 값이 아니라면 arr[i]에 i에 찾아온 인덱스 값을 빼기.
+                arr[i] = i - arrayList.lastIndexOf(s.charAt(i)); // 중복된 값이 아니라면 arr[i]에 i에 찾아온 인덱스 값을 빼기.
             }
+            arrayList.add(s.charAt(i)); // arrayList에 뽑아준 값을 add
         }
         return arr; // 리턴
     }
