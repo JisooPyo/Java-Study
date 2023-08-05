1f4dff1524f58c349bce4bcf794bdfc82a6de0c2
hojooglim <hojooglim@gmail.com>
2023-08-05 22:30:51 +0900

    hojoog_P_K번째수

--- src/Test08/t0807/Hojoong.java
+++ src/Test08/t0807/Hojoong.java
@@ -21,19 +21,13 @@ public class Hojoong {
         //answer.length == commands.length
         int[] answer = new int[commands.length];
 
-        //주어진 int배열의 Array를 substring 하기 위해 arraylist로 변환
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        for (int element : array) {
-            arrayList.add(element);
-        }
-
         //for문을 돌며 commands[i] 에 있는 배열의 i,j,k를 뽑아서 숫자를 구함
         for (int i = 0; i < commands.length; i++) {
 
-
-            //commands[i]번째의 배열을 따로 뽑아서, arrayList와 같이 getNumber 메서드에 보내 로직 수행
+            //commands[i]번째의 배열을 따로 뽑아서, array와 같이 getNumber 메서드에 보내 로직 수행
             int[] command = commands[i];
-            int number = getNumber(arrayList, command);
+            int number = getNumber(array, command);
+
             answer[i] = number;
         }
 
@@ -41,16 +35,17 @@ public class Hojoong {
 
     }
 
-    public static Integer getNumber(ArrayList<Integer> arrayList, int[] command) {
+    public static Integer getNumber(int[] array, int[] command) {
+
         //편의상 command 의 i,j,k 변수 선언
         int startIndex = command[0] - 1;
         int lastIndex = command[1];
         int getIndex = command[2] - 1;
 
-        //for문을 통해 원하는 index 만큼만 substring
+        //for문을 통해 i~j index 만큼의 list 생성
         ArrayList<Integer> list = new ArrayList<>();
         for (int i = startIndex; i < lastIndex; i++) {
-            list.add(arrayList.get(i));
+            list.add(array[i]);
         }
 
         //정렬 후
@@ -60,12 +55,12 @@ public class Hojoong {
         return list.get(getIndex);
     }
 
-//    테스트 1 〉	통과 (0.22ms, 70.6MB)
-//    테스트 2 〉	통과 (0.33ms, 76.7MB)
-//    테스트 3 〉	통과 (0.23ms, 76.2MB)
-//    테스트 4 〉	통과 (0.29ms, 74.7MB)
-//    테스트 5 〉	통과 (0.26ms, 72.4MB)
-//    테스트 6 〉	통과 (0.22ms, 74.8MB)
-//    테스트 7 〉	통과 (0.21ms, 74.5MB)
+//    테스트 1 〉	통과 (0.28ms, 79.2MB)
+//    테스트 2 〉	통과 (0.24ms, 72.5MB)
+//    테스트 3 〉	통과 (0.27ms, 86.3MB)
+//    테스트 4 〉	통과 (0.23ms, 74.3MB)
+//    테스트 5 〉	통과 (0.25ms, 74.1MB)
+//    테스트 6 〉	통과 (0.21ms, 75.2MB)
+//    테스트 7 〉	통과 (0.21ms, 73.7MB)
 
 }
